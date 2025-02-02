1407967fff90bf8f9ad462c31fbd3076f26e8dd8
hoyah <hoyaho03@snu.ac.kr>
2025-02-02 18:52:58 +0900

    feat: initial

--- deploy_mutsha_13th/asgi.py
+++ deploy_mutsha_13th/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for deploy_mutsha_12th project.
+ASGI config for deploy_mutsha_13th project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploy_mutsha_12th.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploy_mutsha_13th.settings')
 
 application = get_asgi_application()
--- deploy_mutsha_13th/wsgi.py
+++ deploy_mutsha_13th/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploy_mutsha_12th.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deploy_mutsha_13th.settings')
 
 application = get_wsgi_application()
