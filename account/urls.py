6527efe8bac48c6e02078b8f7892ea8a781a373a
eileen <eileen914@snu.ac.kr>
2025-05-24 17:09:34 +0900

    Feat Seminar_commit_django

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import SignOutView, SignUpView, SignInView, TokenRefreshView
 
-
-app_name = "account"
+from .views import SignOutView, SignUpView, SignInView, TokenRefreshView, UserInfoView
+ 
+app_name = 'account'
 urlpatterns = [
-    # CBV url path
     path("signup/", SignUpView.as_view()),
     path("signin/", SignInView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
     path("signout/", SignOutView.as_view()),
+    path("info/", UserInfoView.as_view()),
+		## /account/info 를 통해 접근 가능!
 ]
